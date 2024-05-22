c907192506d6d33d49792996ec055d2a3483f464
Chikombe <chikombe.ecy@gmail.com>
2024-05-22 08:10:47 +0000

    5. LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -3,55 +3,56 @@
 100-lfu_cache.py: This module implements an LFU caching system.
 """
 
+from collections import defaultdict
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """
-    LFUCache: Implements a caching system with LFU eviction policy.
-    """
-
     def __init__(self):
-        """
-        Initialize the cache.
-        """
+        """Initialize the cache"""
         super().__init__()
-        self.freq = {}
+        self.freq = defaultdict(int)
         self.use_order = {}
+        self.time = 0
 
     def put(self, key, item):
-        """
-        Add an item to the cache. If the key or item is None, do nothing.
-
-        Args:
-            key: The key under which the item is stored.
-            item: The item to store in the cache.
-        """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.freq[key] += 1
-            else:
-                if len(self.cache_data) >= self.MAX_ITEMS:
-                    self.evict()
-                self.cache_data[key] = item
-                self.freq[key] = 1
-                self.use_order[key] = len(self.use_order)
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        
+        self.time += 1
+        
+        if key in self.cache_data:
             self.cache_data[key] = item
-            self.update_use_order(key)
+            self.freq[key] += 1
+            self.use_order[key] = self.time
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                self.evict()
+            self.cache_data[key] = item
+            self.freq[key] = 1
+            self.use_order[key] = self.time
 
     def get(self, key):
-        """
-        Retrieve an item from the cache by key. If the key is None or the key
-        does not exist, return None.
+        """Retrieve an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        
+        self.time += 1
+        self.freq[key] += 1
+        self.use_order[key] = self.time
+        return self.cache_data[key]
 
-        Args:
-            key: The key for the item to retrieve.
-
-        Returns:
-            The cached item, or None if the key does not exist.
-        """
-        if key is not None and key in self.cache_data:
-            self.freq[key] += 1
-            self.update_use_order(key)
-            return self.cache_data[key]
-        return None
+    def evict(self):
+        """Evict the least frequently used item"""
+        min_freq = min(self.freq.values())
+        lfu_keys = [k for k, v in self.freq.items() if v == min_freq]
+        if len(lfu_keys) == 1:
+            lfu_key = lfu_keys[0]
+        else:
+            lfu_key = min(lfu_keys, key=lambda k: self.use_order[k])
+        
+        del self.cache_data[lfu_key]
+        del self.freq[lfu_key]
+        del self.use_order[lfu_key]
+        print(f"DISCARD: {lfu_key}")
